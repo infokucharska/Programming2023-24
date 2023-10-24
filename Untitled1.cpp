fed70341f1cde7a59243a8e8e64dd076d1ee394c
Anhelina Kucharska (Uczeń) <akucharska558@technikumkreatywne.pl>
2023-10-24 15:02:39 +0200

    Dodaliśmy nowy text i zmienną

---
+++ Untitled1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+ 	short zmienna = 9;
+	cout << "Hello world";
+	cout << endl;
+	cout << "liczba: " << zmienna;
+	
+	
+	return 0;
+
+	
+	
+	
+	
+	
+	
+	
+}
