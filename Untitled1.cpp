07b854181c22110ce1410b78228840dd9f5c2821
Anhelina Kucharska (Uczeń) <akucharska558@technikumkreatywne.pl>
2023-10-25 14:52:50 +0200

    powtorzenie i zadanie

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,11 +2,31 @@
 using namespace std;
 int main()
  {
+ 	// deklaracja zmiennych
  	short zmienna = 9;
+ 	float zmiennoprzecinkowa = 3.21;
+ 	string tekst = "tekst";
+ 	char znak = 'A';
+ 	bool logiczny = false;
+ 	/*	
 	cout << "Hello world";
+	cout << endl;  // koniec linii
+	cout << "liczba: " << zmienna; // wyswietlenie zmiennej
 	cout << endl;
-	cout << "liczba: " << zmienna;
 	
+	string wyraz;
+	cout << "Podaj wyraz";
+	cin >> wyraz;
+	cout << "Podales wyraz: " << wyraz;
+	*/
+	
+	string imie;
+	cout << "Podaj swoje imie ";
+	cin >> imie;
+	short wiek;
+	cout << "Podaj swoj wiek ";
+	cin >> wiek;
+	cout << "Witaj, " << imie << " masz "<< wiek <<" lat";
 	
 	return 0;
 
