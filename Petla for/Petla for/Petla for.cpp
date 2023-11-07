f3f818d90259acf499f34ebfd18e9403d87f2785
Anhelina Kucharska (Uczeń) <akucharska558@technikumkreatywne.pl>
2023-11-07 15:04:25 +0100

    Petla

---
+++ Petla for/Petla for/Petla for.cpp
@@ -0,0 +1,22 @@
+// Petla for.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 5; j++) {
+			cout << " i: " << i;
+			cout << " j: " << j;
+			cout << " | ";
+		}
+		cout << endl;
+	}
+
+
+
+
+
+}
