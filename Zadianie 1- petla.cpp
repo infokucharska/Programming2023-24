b76bc828804d1d069e87de15e4f6fe376c892f4e
infokucharska <kucarskaangelina@gmail.com>
2023-11-08 15:01:56 +0100

    Zadania z petla w petle

---
+++ Zadianie 1- petla.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	for (int i=1; i <= 10; i++){
+		for(int j=1; j<=10; j++){
+			cout << i;
+			cout << " * ";
+			cout << j;
+			cout << " = ";
+			cout << i*j;
+			cout << endl;
+		}
+		cout << endl;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
